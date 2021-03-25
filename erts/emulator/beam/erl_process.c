972031c16556abc6ee7755e4c543a966c5d76632
John Högberg <john@erlang.org>
2021-03-25 11:15:28 +0100

    erts: Refactor and rename beam_apply/error_action_code
    `beam_apply` has only ever been used to start processes and we've
    always manually pushed `beam_normal_exit` as its return address
    when doing so, so `i_apply_only` makes more sense than `i_apply`
    and lets us get rid of a redundant CP when running under native-
    stack JIT. While we're at it, we'll give it a better name:
    `beam_run_process`
    
    Similarly, `error_action_code` is only used for process termination
    and raising `function_clause`. There's very little benefit from
    using it for the latter, so we may as well refactor it not to and
    rename `error_action_code` to `process_exit`.

--- erts/emulator/beam/erl_process.c
+++ erts/emulator/beam/erl_process.c
@@ -12105,7 +12105,7 @@ erl_create_process(Process* parent, /* Parent of process (default group leader).
 
     p->current = &p->u.initial;
 
-    p->i = beam_apply;
+    p->i = beam_run_process;
 
     switch (erts_frame_layout) {
     case ERTS_FRAME_LAYOUT_RA:
